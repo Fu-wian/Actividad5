72ee8cc1e56b756d4123ad1f790e7f2852ce6a26
Jackie Chung <jackie.chung@utp.ac.pa>
2026-06-22 17:28:13 -0500

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    alias(libs.plugins.android.application)
+    id("com.google.devtools.ksp")
+}
+
+android {
+    namespace = "com.example.actividad5"
+    compileSdk {
+        version = release(37) {
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.example.actividad5"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("androidx.activity:activity-ktx:1.8.2")
+
+    implementation("androidx.room:room-runtime:2.7.0")
+    implementation("androidx.room:room-ktx:2.7.0")
+    ksp("androidx.room:room-compiler:2.7.0")
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+}
---
+++ app/src/main/java/com/example/actividad5/repository/EntradaRepository.kt
@@ -0,0 +1,28 @@
+package com.example.actividad5.repository
+
+import android.content.Context
+import com.example.actividad5.data.DiarioDatabase
+import com.example.actividad5.data.Entrada
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class EntradaRepository(context: Context) {
+
+    private val dao = DiarioDatabase.Companion.getInstance(context).entradaDao()
+
+    suspend fun getAll(): List<Entrada> = withContext(Dispatchers.IO) {
+        dao.getAll()
+    }
+
+    suspend fun getById(id: Long): Entrada? = withContext(Dispatchers.IO) {
+        dao.getById(id)
+    }
+
+    suspend fun insertar(entrada: Entrada): Long = withContext(Dispatchers.IO) {
+        dao.insertar(entrada)
+    }
+
+    suspend fun eliminar(entrada: Entrada) = withContext(Dispatchers.IO) {
+        dao.eliminar(entrada)
+    }
+}
